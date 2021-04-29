832c0297e7abdbba4cc289d1e74d0c526362e72a
Tim Hanewich <tahanewich@live.com>
2021-04-29 13:20:45 -0400

    Added email generator

--- src/DataGeneration/DataGenerator.cs
+++ src/DataGeneration/DataGenerator.cs
@@ -8,12 +8,10 @@ namespace PowerSource.DataGeneration
 {
     public class DataGenerator
     {
-        private string[] MaleFirstName;
-        private string[] FemaleFirstName;
-        private string[] LastName;
-
+        
         #region "Setup"
 
+        //Names
         public async Task LoadMaleFirstNamesAsync(Stream file)
         {
             MaleFirstName = await PowerSourceToolkit.DelimitedFileToLineArrayAsync(file);
@@ -29,9 +27,20 @@ namespace PowerSource.DataGeneration
             LastName = await PowerSourceToolkit.DelimitedFileToLineArrayAsync(file);
         }
         
+
+        //Domains
+        public async Task LoadDomainsAsync(Stream file)
+        {
+            Domain = await PowerSourceToolkit.DelimitedFileToLineArrayAsync(file);
+        }
+
         #endregion
 
         #region "Names"
+
+        private string[] MaleFirstName;
+        private string[] FemaleFirstName;
+        private string[] LastName;
 
         #region  "Core"
 
@@ -82,5 +91,106 @@ namespace PowerSource.DataGeneration
         }
     
         #endregion
+    
+        #region "Email"
+
+        private string[] Domain {get; set;}
+
+        //Core
+        public string RandomDomain()
+        {
+            return PowerSourceToolkit.RandomFromArray(Domain);
+        }
+
+        public string RandomEmail()
+        {
+            //Get random first + last name
+            string firstname = "";
+            string lastname = "";
+            try
+            {
+                firstname = RandomFirstName();
+            }
+            catch
+            {
+                throw new Exception("Unable to get random first name to use in random email.");
+            }
+            try
+            {
+                lastname = RandomLastName();
+            }
+            catch
+            {
+                throw new Exception("Unable to get random last name to use in random email.");
+            }
+
+            //Get random domain
+            string randomdomain = "";
+            try
+            {
+                randomdomain = RandomDomain();
+            }
+            catch
+            {
+                throw new Exception("Unable to get random domain for use in random email.");
+            }
+
+            //Email formats
+            //0 = first.last@
+            //1 = last.first@
+            //2 = flast@
+            //3 = last.f@
+            //4 = lastf@
+            //5 = firstl@
+            //6 = firstlast@
+            
+            int rfn = new Random().Next(0, 7);
+
+            //Prepare email part
+            string email = "";
+            if (rfn == 0)
+            {
+                email = firstname + "." + lastname;
+            }
+            else if (rfn == 1)
+            {
+                email = lastname + "." + firstname;
+            }
+            else if (rfn == 2)
+            {
+                email = firstname.Substring(0, 1) + lastname;
+            }
+            else if (rfn == 3)
+            {
+                email = lastname + "." + firstname.Substring(0, 1);
+            }
+            else if (rfn == 4)
+            {
+                email = lastname + firstname.Substring(0, 1);
+            }
+            else if (rfn == 5)
+            {
+                email = firstname + lastname.Substring(0, 1);
+            }
+            else if (rfn == 6)
+            {
+                email = firstname + lastname;
+            }
+            else //random?
+            {
+                email = firstname.Substring(0, 1) + lastname.Substring(0, 1);
+            }
+
+            //Append the domain
+            email = email + "@" + randomdomain;
+
+            //Lower
+            email = email.ToLower();
+
+            return email;
+        }
+
+        #endregion
+    
     }
 }
